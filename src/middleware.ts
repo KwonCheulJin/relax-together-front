22551828f2c9b3505f826194da19b15b6a26eae2
KwonCheulJin <chkftm12@gmail.com>
2024-11-28 20:25:56 +0900

    feat: 로그인 유저 식별자로 사용한 isLoginUser 쿠키를 middleware에서 읽지 못하는 문제로 관련된 로직 주석 처리 및 노트 작성

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const FALLBACK_URL = '/gatherings';
 const FALLBACK_LOGIN_URL = '/signin';
 const FALLBACK_FORGOT_PASSWORD_URL = '/forgot-password';
-const withAuthList = ['/mypage'];
-const withOutAuthList = ['/signin', '/signup', '/reset-password'];
 
-function parseCookies(cookieHeader: string | null): { [key: string]: string } {
-  const cookies: { [key: string]: string } = {};
-  if (cookieHeader) {
-    cookieHeader.split(';').forEach(cookie => {
-      const [name, value] = cookie.split('=').map(c => c.trim());
-      cookies[name] = decodeURIComponent(value);
-    });
-  }
-  return cookies;
-}
+// NOTE: 주석 처리된 부분 브라우저의 보안 정책과 Public Suffix List 때문에 vercel.app 도메인으로 쿠키를 설정할 수 없는 문제가 있어서 middleware 기능 일부 주석처리
+// NOTE: 추후에 도메인을 같이 통일하는 방법으로 수정
+
+// const FALLBACK_URL = '/gatherings';
+// const withAuthList = ['/mypage'];
+// const withOutAuthList = ['/signin', '/signup', '/reset-password'];
+
+// function parseCookies(cookieHeader: string | null): { [key: string]: string } {
+//   const cookies: { [key: string]: string } = {};
+//   if (cookieHeader) {
+//     cookieHeader.split(';').forEach(cookie => {
+//       const [name, value] = cookie.split('=').map(c => c.trim());
+//       cookies[name] = decodeURIComponent(value);
+//     });
+//   }
+//   return cookies;
+// }
 
 const getEmail = async (token: string | null) => {
   const response = await fetch(
@@ -34,22 +38,24 @@ const getEmail = async (token: string | null) => {
 };
 
 export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  const cookies = parseCookies(req.headers.get('cookie'));
-  const isLoginUser = cookies['isLoginUser'];
-
-  const targetPathname = pathname.split('/')[1];
-  const isWithAuth = withAuthList.includes(`/${targetPathname}`);
-  const isWithOutAuth = withOutAuthList.includes(`/${targetPathname}`);
   const url = req.nextUrl.clone();
 
-  if (isWithAuth && isLoginUser !== 'true') {
-    return NextResponse.redirect(new URL(FALLBACK_URL, req.url));
-  }
+  // const { pathname } = req.nextUrl;
+  // const cookies = parseCookies(req.headers.get('cookie'));
 
-  if (isWithOutAuth && isLoginUser === 'true') {
-    return NextResponse.redirect(new URL(FALLBACK_URL, req.url));
-  }
+  // const isLoginUser = cookies['isLoginUser'];
+
+  // const targetPathname = pathname.split('/')[1];
+  // const isWithAuth = withAuthList.includes(`/${targetPathname}`);
+  // const isWithOutAuth = withOutAuthList.includes(`/${targetPathname}`);
+
+  // if (isWithAuth && isLoginUser !== 'true') {
+  //   return NextResponse.redirect(new URL(FALLBACK_URL, req.url));
+  // }
+
+  // if (isWithOutAuth && isLoginUser === 'true') {
+  //   return NextResponse.redirect(new URL(FALLBACK_URL, req.url));
+  // }
 
   if (url.pathname === '/mypage' && !url.searchParams.has('subPage')) {
     url.searchParams.set('subPage', 'my-gatherings');
